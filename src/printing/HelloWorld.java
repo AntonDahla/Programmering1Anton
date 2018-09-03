72c5cc1ca73c53ec37c279d56a6bc87bde80d1c3
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-09-03 12:13:34 +0200

    Hello/Päron/ Java
    idk

--- src/printing/HelloWorld.java
+++ src/printing/HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld {
 
 	public static void main (String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Päron,Päron!");
+		System.out.println("Hello");
+		System.out.println("Anton Dahla");
+		System.out.println(" J    a   v     v  a                                                  \r\n" + 
+				"     J   a a   v   v  a a                                                 \r\n" + 
+				"  J  J  aaaaa   V V  aaaaa                                                \r\n" + 
+				"   JJ  a     a   V  a     a");
 	}
 	
 }
