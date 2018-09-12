2a928944dcf47ad2c447c2a83bc1233aeb4f01fa
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-09-12 14:55:09 +0200

    Printed a face
    Idk Kevin

--- src/printing/Greetings.java
+++ src/printing/Greetings.java
@@ -3,8 +3,22 @@ package printing;
 public class Greetings { 
 
 public static void main (String[] args) {
-	System.out.println("Hello"), "world!");
+	System.out.println("Hello" + "world!");
+	System.out.println("    =    ");
+	System.out.println("    =    ");
+	System.out.println("    =    ");
 	
+	
+	
+	
+	
+    System.out.println(  "++++++");
+    System.out.println("[| o o |]");
+	System.out.println(" |  ^  |");
+	System.out.println(" | '-' |");
+	System.out.println(" +-----+");
+			  
+   
 	
 } 
 	
