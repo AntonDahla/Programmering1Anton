775cb46949ce8383292144298129434ef1a2b595
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-09-14 09:35:43 +0200

    Square root of 81?
    Idk Kev

--- src/printing/numbers/IntroPrintingNumbers.java
+++ src/printing/numbers/IntroPrintingNumbers.java
@@ -4,8 +4,7 @@ public class IntroPrintingNumbers {
 	
 	     public static void main(String[] args) {
 	   
-	        System.out.println(74 + 36);
-	     
+           System.out.println(81 / 9); 
 	     
 	     
 	        
