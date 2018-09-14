885d591897224131edaab371e488038d7c8fcc31
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-09-14 10:25:14 +0200

    Specified Expression

--- src/printing/numbers/IntroPrintingNumbers.java
+++ src/printing/numbers/IntroPrintingNumbers.java
@@ -4,17 +4,7 @@ public class IntroPrintingNumbers {
 	
 	     public static void main(String[] args) {
 	 
-           System.out.println("A:");
-	       System.out.println( -5 + 8 * 6  );
-	       
-	       System.out.println("B:");
-	       System.out.println((55+9)  % 9);
-	       
-	       System.out.println("C:");
-	       System.out.println(20 + -3 * 5 / 8);
-	       
-	       System.out.println("D:");
-	       System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+	    	System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)); 
 	        
 	  }  
 	     
