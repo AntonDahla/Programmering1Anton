ba1ba75ff118f21e8329ea2a7735fcb6899ce561
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-09-14 10:18:14 +0200

    Math at it's finest

--- src/printing/numbers/IntroPrintingNumbers.java
+++ src/printing/numbers/IntroPrintingNumbers.java
@@ -3,10 +3,18 @@ package printing.numbers;
 public class IntroPrintingNumbers {
 	
 	     public static void main(String[] args) {
-	   
-           System.out.println(81 / 9); 
-	     
-	     System.out.println( ":)" );
+	 
+           System.out.println("A:");
+	       System.out.println( -5 + 8 * 6  );
+	       
+	       System.out.println("B:");
+	       System.out.println((55+9)  % 9);
+	       
+	       System.out.println("C:");
+	       System.out.println(20 + -3 * 5 / 8);
+	       
+	       System.out.println("D:");
+	       System.out.println(5 + 15 / 3 * 2 - 8 % 3);
 	        
 	  }  
 	     
