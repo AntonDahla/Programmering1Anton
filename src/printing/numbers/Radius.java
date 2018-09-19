64c4fce45246dafbdde373918209b6354d6d715e
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-09-19 14:09:55 +0200

    Musen mot Råttan, aka Bajen mot Gnaget
    idk Kevin

---
+++ src/printing/numbers/Radius.java
@@ -0,0 +1,11 @@
+package printing.numbers;
+
+public class Radius {
+
+	 
+	public static void main(String[] args) {
+		System.out.println("radius = 7.5;");
+		System.out.println("Area: " + 7.5 * 7.5 * 3.141592);
+		System.out.println("Perimeter: " + (7.5 + 7.5) * 3.141592);
+	}
+}
---
+++ src/variableridentifierare/FirstVariable.java
@@ -0,0 +1,21 @@
+package variableridentifierare;
+
+public class FirstVariable {
+	
+	     
+	public static void main(String[] args) {
+		
+		
+		
+	     int gravityMultiplied	= 0;
+	     boolean bool = true;
+	     boolean fool = false;
+	     
+	     final double GRAVITY_MARS = 15.15;
+	     
+	     System.out.println(gravityMultiplied);
+	     
+	     
+	     
+	}
+}
