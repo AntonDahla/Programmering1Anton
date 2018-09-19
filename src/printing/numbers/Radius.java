18753fbc3399cf1af373c935f8ed2a09020a6587
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-09-19 14:28:51 +0200

    Jonas vadfan gör du på fritiden?
    Idk Kevin

--- src/printing/numbers/Radius.java
+++ src/printing/numbers/Radius.java
@@ -4,8 +4,9 @@ public class Radius {
 
 	 
 	public static void main(String[] args) {
-		System.out.println("radius = 7.5;");
-		System.out.println("Area: " + 7.5 * 7.5 * 3.141592);
-		System.out.println("Perimeter: " + (7.5 + 7.5) * 3.141592);
-	}
+		System.out.println("Width = 5.5;");
+		System.out.println("Height = 8.5;");
+        System.out.println("Area: " + 5.5 * 8.5);
+		System.out.println("Perimeter: " + 2 * (5.5 + 8.5));
+	}	
 }
