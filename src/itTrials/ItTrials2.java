a3a2e067ba665ab26859be5a2d3af74bc3bf1a49
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-10-03 14:18:17 +0200

    It Trials uppgift 2
    Idk Kevin

---
+++ src/itTrials/ItTrials2.java
@@ -0,0 +1,15 @@
+package itTrials;
+
+public class ItTrials2 {
+              
+	public static void main(String[] args) {
+		  double value = 12.56;
+		  double fractional_part = value % 1;
+		  double integral_part = value - fractional_part;  
+		  System.out.print("\nOriginal value: "+value);
+		  System.out.print("\nIntegral part: "+integral_part);
+		  System.out.print("\nFractional part: "+fractional_part);
+		  System.out.println();  
+		  }
+}
+
