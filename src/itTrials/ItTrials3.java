a2ae569d5250498609e2b36788d6750e0e88a64a
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-10-05 09:38:33 +0200

    ItTrials4

---
+++ src/itTrials/ItTrials3.java
@@ -0,0 +1,23 @@
+package itTrials;
+
+public class ItTrials3 {
+
+	public static boolean isInteger(double number){
+	    return Math.ceil(number) == Math.floor(number); 
+	}
+        
+
+
+
+
+
+
+}
+		    
+	   
+
+	
+	
+
+
+
---
+++ src/loops/ForLoop.java
@@ -0,0 +1,29 @@
+package loops;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		 /*
+		int number = 0;
+		
+		System.out.println(number);
+		number = number + 1;
+		System.out.println(number);
+		number += number;
+		System.out.println(number);
+		number++;
+		System.out.println(number);
+		*/
+		
+		/*
+		 * for ( startvärde; slutvärde; förändring; )
+		 */
+		
+		for(int i = 1000; i >= 800;i --) {
+		    System.out.println(i);
+			
+		}
+
+	}
+
+}
