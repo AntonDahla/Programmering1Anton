9efbd42a36a50fa6133afb6e48e16a78217865a8
AntonDahla <anton.dahla@elev.ga.ntig.se>
2018-10-24 14:33:51 +0200

    Update src/functions/Uppgift1Funktioner.java

--- src/functions/Uppgift1Funktioner.java
+++ src/functions/Uppgift1Funktioner.java
@@ -5,39 +5,71 @@ import java.util.Scanner;
 public class Uppgift1Funktioner {
 
 	public static void main(String[] args) {
-     largestAmongThree (input());	
+		smallestAmongThree(input() );
 
 	}
-	
-	public static int [] input() {
+	public static int[] input() {
 		Scanner scanner = new Scanner(System.in);
 		int[] inputs = new int[3];
-		System.out.println("Input first number:");
+		System.out.print("Input your first number: ");
 		inputs[0] = scanner.nextInt();
-		System.out.println("Input the second number:");
+		System.out.print("Input your second number: ");
 		inputs[1] = scanner.nextInt();
-		System.out.println("Input the third and final number:");
+		System.out.print("Input your third number: ");
+		inputs[2] = scanner.nextInt();
+		return inputs;
+	}
+	
+	public static void smallestAmongThree(int[] inputs) {
+		int tempVar;
+		if (inputs[0] < inputs[1]) {
+			tempVar = inputs[0];
+		} else {
+			tempVar = inputs[1];
+		}
+		System.out.println("And your smallest number is: ");
+		if (tempVar < inputs[2]) {
+			System.out.println(tempVar);
+		} else {
+			System.out.println(inputs[2]);
+		}
+		largestAmongThree(bigput() );
+
+	}
+	public static int[] bigput() {
+		Scanner scanner = new Scanner(System.in);
+		int[] inputs = new int[3];
+		System.out.print("Input your first number: ");
+		inputs[0] = scanner.nextInt();
+		System.out.print("Input your second number: ");
+		inputs[1] = scanner.nextInt();
+		System.out.print("Input your third number: ");
 		inputs[2] = scanner.nextInt();
 		return inputs;
 	}
 	
 	public static void largestAmongThree(int[] inputs) {
-		int tempVar;
-		if (inputs[0] > inputs[1]) {
-			tempVar = inputs[0];
-		} else { 
-			tempVar = inputs[1];
 		
-		}
-		if (tempVar > inputs[2]) {
-			System.out.println(tempVar);
-		}
+		int tempVar;
+		if (inputs[0] < inputs[1]) {
 			
-	}}
-	
-	
-	
+			tempVar = inputs[1];
+			
+		} else {
+			
+			tempVar = inputs[0];
+		}
+		
+		System.out.println("And your biggest number is: ");
+		
+		if (tempVar > inputs[2]) {
+			
+			System.out.println(tempVar);
+			
+		} else {
+			
+			System.out.println(inputs[2]);
+		}
+	}
 
-
-	
-	
+}
